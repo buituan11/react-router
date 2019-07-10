c730ca7694044979623af0a762c4b6e8a982f8b2
buituan11 <buituan71099@gmail.com>
2019-07-10 23:34:19 +0700

    update loi

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Users from './User.jsx';
 
 function Index() {
   return (
-    <Router>
-      <Link to="/users/">Users</Link>
-      <Route path="/users/" component={Users} />  
-    </Router>
+      <Link to="/users/">Ấn vào link chuyển sang trang User</Link>
   );
 }
 
 function About() {
   return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 function App() {
@@ -38,8 +32,8 @@ function App() {
         </nav>
 
         <Route path="/" exact component={Index} />
-        <Route path="/about/" component={About} />
-        <Route path="/users/" component={Users} />
+        <Route path="/about/"  component={About} />
+        <Route path="/users/"  component={Users} />
       </div>
     </Router>
   );
---
+++ src/User.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h2>Users</h2>
+        );
+    }
+}
+
+export default Users;
